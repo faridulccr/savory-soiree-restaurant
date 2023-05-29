ab7c83c2df9a295ad4710e19ca84fffc3d623898
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-05-30 01:23:29 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+// components
+import App from "./App.jsx";
+import ErrorPage from "./ErrorPage";
+import Dashboard from "./routes/Dashboard/Dashboard";
+import Home from "./routes/Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/dashboard",
+                element: <Dashboard />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
