1dd23bf95854f9e16ead9208977eb3482e4dd4ea
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-05-30 02:21:46 +0600

    tailwind and daisy ui setup

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Home</h1>
+            <button className="btn">Button</button>
+            <button className="btn btn-primary">Button</button>
+            <button className="btn btn-secondary">Button</button>
+            <button className="btn btn-accent">Button</button>
+            <button className="btn btn-ghost">Button</button>
+            <button className="btn btn-link">Button</button>
+        </div>
+    );
 };
 
 export default Home;
