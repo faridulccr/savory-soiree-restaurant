f96c149ee2bff63cb7fca08a5a1fdc6c21460179
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-06-02 14:49:18 +0600

    doing slider responsive

--- src/components/heading/Heading.jsx
+++ src/components/heading/Heading.jsx
@@ -1,10 +1,10 @@
 const Heading = ({ text, title }) => {
     return (
         <div className="mt-16">
-            <p className=" italic text-[#D99904] text-lg text-center">
+            <p className="italic text-[#D99904] text-lg text-center">
                 ---{text}---
             </p>
-            <h2 className=" w-[424px] mx-auto border-y-4 mt-5 mb-12 font-['Inter'] text-4xl text-center py-4">
+            <h2 className="w-2/3 sm:w-1/2 mx-auto border-y-4 mt-5 mb-12 font-['Inter'] text-xl sm:text-3xl md:text-4xl text-center py-4">
                 {title}
             </h2>
         </div>
--- src/routes/Home/Slider.jsx
+++ src/routes/Home/Slider.jsx
@@ -27,34 +27,59 @@ const Slider = () => {
             >
                 <SwiperSlide>
                     <img src={slider1} alt="" />
+                    <h3 className=" text-slate-600 hidden sm:block text-3xl md:text-4xl text-center font-['Cinzel'] -mt-16 w-fit mx-auto">
+                        SALADS
+                    </h3>
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src={slider2} alt="" />
+                    <h3 className=" text-slate-600 hidden sm:block text-3xl md:text-4xl text-center font-['Cinzel'] -mt-16 w-fit mx-auto">
+                        PIZZAS
+                    </h3>
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src={slider3} alt="" />
+                    <h3 className=" text-slate-600 hidden sm:block text-3xl md:text-4xl text-center font-['Cinzel'] -mt-16 w-fit mx-auto">
+                        SOUPS
+                    </h3>
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src={slider4} alt="" />
+                    <h3 className=" text-slate-600 hidden sm:block text-3xl md:text-4xl text-center font-['Cinzel'] -mt-16 w-fit mx-auto">
+                        CAKES
+                    </h3>
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src={slider5} alt="" />
+                    <h3 className=" text-slate-600 hidden sm:block text-3xl md:text-4xl text-center font-['Cinzel'] -mt-16 w-fit mx-auto">
+                        SALADS
+                    </h3>
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src={slider3} alt="" />
+                    <h3 className=" text-slate-600 hidden sm:block text-3xl md:text-4xl text-center font-['Cinzel'] -mt-16 w-fit mx-auto">
+                        SOUPS
+                    </h3>
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src={slider2} alt="" />
+                    <h3 className=" text-slate-600 hidden sm:block text-3xl md:text-4xl text-center font-['Cinzel'] -mt-16 w-fit mx-auto">
+                        PIZZAS
+                    </h3>
                 </SwiperSlide>
             </Swiper>
 
             <div className="mt-16 relative">
-                <img className="w-full h-full" src={chefImg} alt="" />
-                <div className="w-3/4 text-center py-20 bg-white bg- absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+                <img
+                    className="w-full h-[400px] sm:h-[500px]"
+                    src={chefImg}
+                    alt=""
+                />
+                <div className="w-11/12 sm:w-3/4 text-center py-10 sm:py-20 bg-white bg- absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
                     <h3 className="font-[400] font-['Cinzel'] mb-2 text-3xl text-[#151515]">
                         SAVORY SOIREE
                     </h3>
-                    <p className="w-2/3 mx-auto text-[#151515] font-['Inter']">
+                    <p className=" px-2 sm:px-0 sm:w-2/3 mx-auto text-[#151515] font-['Inter']">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Necessitatibus, libero accusamus laborum deserunt
                         ratione dolor officiis praesentium! Deserunt magni
